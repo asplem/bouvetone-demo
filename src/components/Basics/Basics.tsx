595f01b6b353bbcc97dde4ab844692edaa310945
Mari Asplem <mariasplem@gmail.com>
2022-10-19 12:19:14 +0200

    More stuff

---
+++ src/components/Basics/Basics.tsx
@@ -0,0 +1,13 @@
+import { Container } from '@mantine/core';
+
+export const Basics = () => {
+  return (
+    <Container>
+      <h1>Basics</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus maiores quos illum eligendi fugit cum saepe
+        minima iusto quibusdam, provident numquam dolorum, consequatur vel sit? Tenetur laborum possimus mollitia ab!
+      </p>
+    </Container>
+  );
+};
